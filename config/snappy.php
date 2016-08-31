604ec3c066b13f30c44716468dca17793905ae2a
Evaldo <evaldoavelar@gmail.com>
2016-08-30 21:07:27 -0300

    Ajuste PDF Linux

--- config/snappy.php
+++ config/snappy.php
@@ -1,20 +1,43 @@
 <?php
 
-return array(
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+
+    return array(
 
 
-    'pdf' => array(
-        'enabled' => true,
-        'binary' =>'C:\wkhtmltopdf\bin\wkhtmltopdf', // '/usr/local/bin/wkhtmltopdf',
-        'timeout' => false,
-        'options' => array(),
-    ),
-    'image' => array(
-        'enabled' => true,
-        'binary' =>'C:\wkhtmltopdf\bin\wkhtmltoimage', // '/usr/local/bin/wkhtmltoimage',
-        'timeout' => false,
-        'options' => array(),
-    ),
+        'pdf' => array(
+            'enabled' => true,
+            'binary' => 'C:\wkhtmltopdf\bin\wkhtmltopdf', // '/usr/local/bin/wkhtmltox/bin/wkhtmltopdf',
+            'timeout' => false,
+            'options' => array(),
+        ),
+        'image' => array(
+            'enabled' => true,
+            'binary' => 'C:\wkhtmltopdf\bin\wkhtmltoimage', // '/usr/local/bin/wkhtmltox/bin/wkhtmltoimage',
+            'timeout' => false,
+            'options' => array(),
+        ),
 
 
-);
+    );
+
+}else{
+    return array(
+
+
+        'pdf' => array(
+            'enabled' => true,
+            'binary' =>  '/usr/local/bin/wkhtmltox/bin/wkhtmltopdf',
+            'timeout' => false,
+            'options' => array(),
+        ),
+        'image' => array(
+            'enabled' => true,
+            'binary' => '/usr/local/bin/wkhtmltox/bin/wkhtmltoimage',
+            'timeout' => false,
+            'options' => array(),
+        ),
+
+
+    );
+}
